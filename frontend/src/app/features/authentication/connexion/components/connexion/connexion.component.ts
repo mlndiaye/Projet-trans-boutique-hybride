a54698b1378e05637f8cb7499f1ece4f9336d8f6
MohCw <mohamedcamaraj@gmail.com>
2024-09-02 16:44:42 +0000

    authentication service implementation

--- frontend/src/app/features/authentication/connexion/components/connexion/connexion.component.ts
+++ frontend/src/app/features/authentication/connexion/components/connexion/connexion.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../../auth.service';
+import { Router } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-connexion',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './connexion.component.html',
   styleUrl: './connexion.component.css'
 })
 export class ConnexionComponent {
+  authService = inject(AuthService);
+  router = inject(Router);
 
+  protected loginForm = new FormGroup({
+    email: new FormControl('', [Validators  .required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  })
+
+  onSubmit(){
+    if(this.loginForm.valid){
+      console.log(this.loginForm.value);
+      this.authService.login(this.loginForm.value)
+      .subscribe((data: any) => {
+        if(this.authService.isLoggedIn()){
+          this.router.navigate(['/']);
+        }
+        console.log(data);
+      });
+    }
+  }
 }
--- frontend/src/app/features/authentication/inscription/components/inscription/inscription.component.ts
+++ frontend/src/app/features/authentication/inscription/components/inscription/inscription.component.ts
@@ -1,26 +1,42 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../../auth.service';
 
 @Component({
   selector: 'app-inscription',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './inscription.component.html',
   styleUrl: './inscription.component.css'
 })
 export class InscriptionComponent {
   inscriptionForm: FormGroup;
+  authService = inject(AuthService);
+  router = inject(Router);
 
   constructor(private formBuilder: FormBuilder) {
     this.inscriptionForm = this.formBuilder.group({
-      nom : ['', Validators.required],
-      prenom: ['', Validators.required],
-      email: ['', Validators.email],
-      mot_de_passe: ['', Validators.required, Validators.minLength(8)],
-      confirmation_mot_de_passe: ['', Validators.required]
+      first_name : ['', Validators.required],
+      last_name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      confirm_password: ['', [Validators.required, Validators.minLength(8)]]
     });
-    
+  }
+
+  public onSubmit() {
+    if (this.inscriptionForm.valid) {
+      console.log(this.inscriptionForm.value);
+      this.authService.signup(this.inscriptionForm.value)
+        .subscribe({
+          next: (data: any) => {
+            console.log(data);
+            this.router.navigate(['/connexion']);
+          },
+          error: (err) => console.log(err)
+        });
+    }
   }
 
 }
